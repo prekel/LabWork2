db0d739c32c956da9cd2ac6b567a902941e9bb01
Vladislav Prekel <misterptits@yandex.ru>
2018-09-12 00:22:13 +0700

    more tests

--- LabWork2.Lib.Tests/test.cpp
+++ LabWork2.Lib.Tests/test.cpp
@@ -5,5 +5,10 @@ extern "C" {
 
 TEST(TestCaseName, SerialResistTest1) {
 	double d[3] = { 1, 2, 3 };
-	ASSERT_EQ(6, serial_resist(3, d));
+	ASSERT_DOUBLE_EQ(6, serial_resist(3, d));
+}
+
+TEST(TestCaseName, ParallelResistTest1) {
+	double d[3] = { 1, 2, 3 };
+	ASSERT_DOUBLE_EQ(1 / (1 + 1.0 / 2 + 1.0 / 3), parallel_resist(3, d));
 }
