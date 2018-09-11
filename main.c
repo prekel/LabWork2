0a3f1fc38f36aed04d91c8e9aa41f016b4f28d82
Vladislav Prekel <misterptits@yandex.ru>
2018-09-11 13:22:26 +0700

    initial
    Signed-off-by: Vladislav Prekel <misterptits@yandex.ru>

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+double serial_resist(int n, const double a[]) {
+    double sum = 0;
+    for (int i = 0; i < n; i++) sum += a[i];
+    return sum;
+}
+
+double parallel_resist(int n, const double a[]) {
+    double prod = 1;
+    for (int i = 0; i < n; i++) prod *= a[i];
+    double sum = 0;
+    for (int i = 0; i < n; i++) sum += prod / a[i];
+    return prod / sum;
+}
+
+int main() {
+    double resist[1000];
+    printf("N: ");
+    int n;
+    scanf("%d", &n);
+    char string_number[100];
+    for (int i = 0; i < n; i++) {
+        while (1) {
+            printf("R n%d: ", i + 1);
+            scanf("%s", string_number);
+            int code = sscanf(string_number, "%lf", &resist[i]);
+            if (resist[i] == 0) continue;
+            if (code > 0) break;
+        };
+    }
+
+    printf("R serial:   %lf\n", serial_resist(n, resist));
+    printf("R parallel: %lf\n", parallel_resist(n, resist));
+    fflush(stdout);
+
+    return 0;
+}
