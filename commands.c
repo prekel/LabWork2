3363efd05aa5f42ee895aca1777a627ebc49ef47
Vladislav Prekel <misterptits@yandex.ru>
2018-09-16 00:19:29 +0700

    help cmd info

--- commands.c
+++ commands.c
@@ -6,21 +6,36 @@
 
 struct command commands[COMMANDS_COUNT] = {
 		{"help",            {"Вывод справки",                    "Print help"},
-				{"Без аргумента: Выводит описание всех команд\nС аргументом (help <имя_команды>): Выводит подробное описание команды\n", ""}},
+				{"Без аргумента: Выводит описание всех команд\nС аргументом (help <имя_команды>): Выводит подробное описание команды\n",
+						"Without argument: Print all commands description\nWith argument (help <cmd_name>): Print detailed info about command]\n"}},
+
 		{"setsize",         {"Установка размера массива",        "Set array size"},
-				{"11", "12"}},
+				{"11",
+						"12"}},
+
 		{"fillmanual",      {"Ввод значений вручную",            "Entering values manually"},
-				{"21", "22"}},
+				{"21",
+						"22"}},
+
 		{"fillrandom",      {"Заполнение случайными значениями", "Filling with random values"},
-				{"31", "32"}},
+				{"31",
+						"32"}},
+
 		{"calculateresist", {"Вычислить сопротивление",          "Calculate resistance"},
-				{"41", "42"}},
+				{"41",
+						"42"}},
+
 		{"print",           {"Печать массива",                   "Print array"},
-				{"51", "52"}},
+				{"51",
+						"52"}},
+
 		{"switchlang",      {"Switch language to english",       "Переключить язык на русский"},
-				{"61", "62"}},
+				{"61",
+						"62"}},
+
 		{"exit",            {"Выход из программы",               "Exiting the program"},
-				{"71", "72"}},
+				{"71",
+						"72"}},
 };
 
 void help_void(int lang) {
