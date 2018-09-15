b301b49dafd23c4f84281352bd2fccc5d921e37d
Vladislav Prekel <misterptits@yandex.ru>
2018-09-15 17:33:41 +0700

    add help

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdbool.h>
+#include <string.h>
 #include "../LabWork2.Lib/functions.h"
 
-int cycle_input_int(char *output, bool(*checker)(int))
-{
+#define MAX_STRING_LENGTH 100
+#define COMMANDS_COUNT 8
+
+int cycle_input_int(char *output, bool(*checker)(int)) {
 	int n;
 	char string_number[100];
 	while (1) {
@@ -17,8 +20,7 @@ int cycle_input_int(char *output, bool(*checker)(int))
 	return n;
 }
 
-double cycle_input_double(char *output, bool(*checker)(double))
-{
+double cycle_input_double(char *output, bool(*checker)(double)) {
 	double n;
 	char string_number[100];
 	while (1) {
@@ -31,12 +33,42 @@ double cycle_input_double(char *output, bool(*checker)(double))
 	return n;
 }
 
+struct command {
+	char *name, *description_ru, *description_en;
+};
+
 int main() {
 	int n;
 	char output[100];
+
+	struct command commands[COMMANDS_COUNT] = {
+			{"help",            "Вывод справки",                           "Print help"},
+			{"setsize",         "Установка размера массива",               "Set array size"},
+			{"fillmanual",      "Установка значений вручную через пробел", ""},
+			{"fillrandom",      "4",                                       ""},
+			{"calculateresist", "5",                                       ""},
+			{"print",           "6",                                       ""},
+			{"switchlang",      "7",                                       ""},
+			{"exit",            "Выход из программы",                      ""},
+	};
+
+	char string_command[MAX_STRING_LENGTH];
+	printf("Введите help для справки\n");
+	while (true) {
+		printf("> ");
+		scanf("%s", string_command);
+		if (strcmp(string_command, commands[0].name) == 0) {
+			for (int i = 0; i < COMMANDS_COUNT; i++) {
+				printf("%s - %s\n", commands[i].name, commands[i].description_ru);
+			}
+		}
+		if (strcmp(string_command, commands[7].name) == 0) {
+			return 0;
+		}
+	}
 	n = cycle_input_int("N: ", checkerN);
 
-	double *resist = (double*)malloc(n * sizeof(double));
+	double *resist = (double *) malloc(n * sizeof(double));
 	for (int i = 0; i < n; i++) {
 		sprintf(output, "R n%d: ", i + 1);
 		resist[i] = cycle_input_double(output, checkerResist);
@@ -46,6 +78,7 @@ int main() {
 	printf("R parallel: %15.6lf\n", parallel_resist(n, resist));
 	fflush(stdout);
 	free(resist);
+	//}
 
 	return 0;
 }
