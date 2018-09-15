e7a4c3064e1151c21ac8bd51c7b0d6790a7d9bd5
Vladislav Prekel <misterptits@yandex.ru>
2018-09-15 09:03:15 +0700

    input n

--- main.c
+++ main.c
@@ -3,16 +3,22 @@
 
 int main() {
 	int n;
-	printf("N: ");
-	scanf("%d", &n);
 	char string_number[100];
+	while (1) {
+		printf("N: ");
+		scanf("%s", string_number);
+		int code = sscanf(string_number, "%d", &n);
+		if (n <= 0) continue;
+		if (code > 0) break;
+	};
+
 	double resist[1000];
 	for (int i = 0; i < n; i++) {
 		while (1) {
 			printf("R n%d: ", i + 1);
 			scanf("%s", string_number);
 			int code = sscanf(string_number, "%lf", &resist[i]);
-			if (resist[i] == 0) continue;
+			if (resist[i] <= 0) continue;
 			if (code > 0) break;
 		};
 	}
